5132f3e89b10f846b254938af9dc1436d14007e6
ccventes <regaliasventes@gmail.com>
2021-11-14 19:56:48 -0500

    cambio de puerto

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 app.get('/',(req,res)=> res.send('<h1>Hello world with express y HTML!</h1>'))
 
-app.listen(3000);
+app.listen(8080);
 console.log('server on');
