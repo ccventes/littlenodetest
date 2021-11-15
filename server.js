8919447c4b05810cffcdd931b6bb6f23324c9b5a
ccventes <regaliasventes@gmail.com>
2021-11-14 19:41:38 -0500

    texto modificado

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.get('/',(req,res)=> res.send('Hello world with express!'))
+app.get('/',(req,res)=> res.send('<h1>Hello world with express y HTML!</h1>'))
 
 app.listen(3000);
 console.log('server on');
